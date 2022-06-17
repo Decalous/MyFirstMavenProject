8983737d4a8f7fbd13e37a5438424ba67857b02f
Decalous <pianoplayer9797@gmail.com>
2022-06-17 16:22:20 -0400

    Made my first test, similar to the included test in the sample code. Learned that assertThat(title).contains("something") is case sensitive.

--- src/test/groovy/MyFirstTest.groovy
+++ src/test/groovy/MyFirstTest.groovy
@@ -1,42 +1,42 @@
 // docs: https://bonigarcia.dev/webdrivermanager/
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager
 
 class ChromeTest {
 
-    WebDriver driver;
+    WebDriver driver
 
     @BeforeAll
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup()
     }
 
     @BeforeEach
     void setupTest() {
-        driver = new ChromeDriver();
+        driver = new ChromeDriver()
     }
 
     @AfterEach
     void teardown() {
-        driver.quit();
+        driver.quit()
     }
 
     @Test
     void test() {
         // Exercise
-        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
-        String title = driver.getTitle();
+        driver.get("https://www.webstaurantstore.com/")
+        String title = driver.getTitle()
 
         // Verify
-        assertThat(title).contains("Selenium WebDriver");
+        assertThat(title).contains("WebstaurantStore")
     }
 
 }
